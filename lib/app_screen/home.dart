2f8e3793134c2123e9a754e8c6cb30502b745905
Debroy, Anirban <Anirban.Debroy@atkinsglobal.com>
2019-06-18 16:28:07 +0530

    Changes

--- lib/app_screen/home.dart
+++ lib/app_screen/home.dart
@@ -11,9 +11,12 @@ class Home extends StatelessWidget{
         color: Colors.deepPurple,
         //decoration: BoxDecoration(border: Border.all(color:Colors.white ),color: Colors.deepPurple,borderRadius: BorderRadius.circular(5.0)),
         //margin: EdgeInsets.all(15.0),
-        width: 200.0,
-        height: 100.0,
-        child: Text("Flight",textDirection: TextDirection.ltr,),
+        //width: 200.0,
+        //height: 100.0,
+        child: Text("Flight",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(decoration: TextDecoration.none,fontSize: 75.0,fontFamily: 'Roboto',fontWeight: FontWeight.w300),
+        ),
       ) ,
     );
 
