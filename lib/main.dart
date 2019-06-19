5c632790a156ed9e1e756eee5bf7f4b23bd45f4f
Debroy, Anirban <Anirban.Debroy@atkinsglobal.com>
2019-06-19 11:12:51 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,29 @@ void main() {
   ));
 }
 
+
+List<String> getListElements(){
+
+  var items= List<String>.generate(1000, (counter)=> "Item $counter");
+
+  return items;
+}
+
 Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful view !"),
-        trailing: Icon(Icons.wb_sunny),
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-    ],
+
+  var listItems= getListElements();
+
+  var listView = ListView.builder(
+   itemBuilder: (context,index){
+
+     return ListTile(
+       leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+       onTap: (){
+         debugPrint('${listItems[index]} was tapped');
+       },
+     );
+   }
   );
 
   return listView;
