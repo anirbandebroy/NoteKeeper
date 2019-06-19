e69b434300d3822316afef60a8a04b0cb41b4557
Debroy, Anirban <Anirban.Debroy@atkinsglobal.com>
2019-06-19 11:58:03 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
 import './app_screen/home.dart';
 
-
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI widget",
     home: Scaffold(
-      appBar: AppBar(title: Text("Basic List view"),) ,
+      appBar: AppBar(
+        title: Text("Basic List view"),
+      ),
       body: getListView(),
     ),
   ));
 }
 
-
-List<String> getListElements(){
-
-  var items= List<String>.generate(100, (counter)=> "Item $counter");
+List<String> getListElements() {
+  var items = List<String>.generate(100, (counter) => "Item $counter");
 
   return items;
 }
 
 Widget getListView() {
-
-  var listItems= getListElements();
+  var listItems = getListElements();
 
   var listView = ListView.builder(
-      itemCount:listItems.length ,
-   itemBuilder: (context,index){
-
-     return ListTile(
-       leading: Icon(Icons.arrow_right),
-      title: Text(listItems[index]),
-       onTap: (){
-         debugPrint('${listItems[index]} was tapped');
-       },
-     );
-   }
-  );
+      itemCount: listItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text(listItems[index]),
+          onTap: () {
+            debugPrint('${listItems[index]} was tapped');
+          },
+        );
+      });
 
   return listView;
 }
