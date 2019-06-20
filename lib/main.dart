5f7c6077fc75bf15d96f5cc3fb69c03fb7fbb35d
Debroy, Anirban <Anirban.Debroy@atkinsglobal.com>
2019-06-20 11:56:24 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,49 @@
 import 'package:flutter/material.dart';
-import './app_screen/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widget",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Basic List view"),
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            debugPrint("FAB Clicked");
-          },
-          child: Icon(Icons.add),
-          tooltip: 'Add one more Item',
-      ),
-    ),
+    title: "Stateful App example",
+    home: FavoriteCity(),
   ));
 }
 
-void showSnackBar(BuildContext context,String item){
-  var snackBar = SnackBar(
-    content: Text("You just tapped  $item"),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: (){
-          debugPrint('performing undo');
-        }),
-  );
-  
-  Scaffold.of(context).showSnackBar(snackBar);
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
 }
 
-List<String> getListElements() {
-  var items = List<String>.generate(100, (counter) => "Item $counter");
+class _FavoriteCityState extends State<FavoriteCity> {
+  String nameCity = "";
 
-  return items;
-}
-
-Widget getListView() {
-  var listItems = getListElements();
-
-  var listView = ListView.builder(
-      itemCount: listItems.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(Icons.arrow_right),
-          title: Text(listItems[index]),
-          onTap: () {
-            showSnackBar(context,listItems[index]);
-          },
-        );
-      });
-
-  return listView;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful App example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  nameCity = userInput;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Your best city is $nameCity",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
